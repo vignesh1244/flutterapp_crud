bd0c36472bd596b2b61cf7961a2e1d95009e39b5
ai-mac4 <vim>
2023-06-21 12:49:09 +0530

    first cmt

---
+++ lib/home.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class MyWidget extends StatefulWidget {
+  @override
+  _MyWidgetState createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  List<dynamic> products = [];
+
+  Future<void> fetchData() async {
+
+    print('hello how are you');
+    final response = await http.get(Uri.parse('http://192.168.0.105:3000/product'));
+    print('rrrr'+response.statusCode.toString());
+    if (response.statusCode == 200) {
+      setState(() {
+        products = json.decode(response.body);
+      });
+    } else {
+      print('Failed to fetch data');
+    }
+  }
+
+  @override
+  void initState() {
+    print('init print');
+    super.initState();
+    fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('hello');
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text('Product List'),
+      ),
+      body:/* Text('hhhh'),*/ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (BuildContext context, int index) {
+          final product = products[index];
+          return ListTile(
+            leading: Image.network(product['image']),
+            title: Text(product['name']),
+            subtitle: Text('Quantity: ${product['quantity']}'),
+            trailing: Text('Price: \$${product['price']}'),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:nodeflutter/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  Firstpage(),
+    );
+  }
+}
+
+
+class Firstpage extends StatefulWidget {
+  const Firstpage({Key? key}) : super(key: key);
+
+  @override
+  State<Firstpage> createState() => _FirstpageState();
+}
+
+class _FirstpageState extends State<Firstpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('node '),),
+      body: TextButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => MyWidget(),
+            ),
+          );
+          // Add your button click logic here
+          print('Button clicked!');
+        },
+        child: Center(
+
+          child: Text(
+            'Click Me',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+          padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+            EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
