2ef7a6e7f382f309918d7265d7989456578edc6a
ai-mac4 <vim>
2023-07-10 13:00:09 +0530

    cmt1

--- lib/model/productmodel.dart
+++ lib/model/productmodel.dart
@@ -1,8 +1,1 @@
-import 'package:rxdart/rxdart.dart';
-class  productcontroller{
-  final _activityIndicatorStreamController = BehaviorSubject<bool>.seeded(false);
-  Stream<bool> get activityIndicatorStream => _activityIndicatorStreamController.stream;
-  Function(bool)? get _setActivityIndicatorStream =>
-      _activityIndicatorStreamController.isClosed ? null : _activityIndicatorStreamController.sink.add;
 
-}
